013849b2555fc4ffb803aa852b410cd5c7d508ed
mjsri <srikanthchowdary1994@gmail.com>
2024-07-16 19:21:22 +0530

    worked on roboshop ec2 instances

--- roboshop.sh
+++ roboshop.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-AMI=ami-03265a0778a880afb
-SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
+AMI=ami-0b4f379183e5706b9
+SG_ID=sg-0ff003356a354c08c
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
-DOMAIN_NAME="daws76s.online"
+ZONE_ID=
+DOMAIN_NAME=srikanthdevops.online
 
 for i in "${INSTANCES[@]}"
-do
-    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-    then
+do  
+    if [ $i=="mongodb" ] || [ $i="mysql"] || [ $i="shipping"]
+    then    
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID 
+    --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=$i}]" --query 'Instance[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
-    #create R53 record, make sure you delete existing record
+    #create route53 records make sure you delete existing records
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
